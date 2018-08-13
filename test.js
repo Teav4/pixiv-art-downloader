463245311d5e40a1e4c62a7993ca36f0892b0b15
lill <teav4@feadorra.com>
2018-08-13 12:16:28 +0700

    new pull

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
 const pixiv = require("./app");
-const fs = require("fs");
-let userid = 2345928
 
-function downloadAll(userid,n){
-    pixiv.getUserWork(userid,n,JSON.parse(fs.readFileSync("account.json","utf-8"))).then(res => {
-        pixiv.download(res);
-        return res
-    }).then(res => {
-        if(res != "limited"){
-            downloadAll(userid,n+30);
-        }
-    });
-}
-downloadAll(userid,0);
-//pixiv.getUserWork(userid,0).then(console.log);
+pixiv.downloadMangaSeries("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=70142396");
